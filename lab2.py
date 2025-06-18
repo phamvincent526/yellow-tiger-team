515b1d8cc40b5968688087ccd44a391c81029792
phamvincent526 <phamvincent09@gmail.com>
2025-06-18 10:59:07 -0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,13 @@
+name = "Vincent"
+nickname = "Vincent"
+Age = 16
+UsedPython = True
+Hobbies = ["Reading", "Gaming", "Volleyball"]
+Favorites = [ "music", "games", "pets"]
+
+print("Users name is" , name, "preferred name is", nickname, "is", Age)
+print("Used python before:", UsedPython)
+print("Hobbies:", Hobbies)
+print("Favorites:", Favorites)
+
+      
